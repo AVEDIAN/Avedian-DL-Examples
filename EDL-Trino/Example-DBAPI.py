8b2d35e9cb0dbd72518d8a13353476a368abd6cd
Gaby RM <info@gabyrm.com>
2022-08-05 12:53:47 -0300

    Init example

---
+++ EDL-Trino/Example-DBAPI.py
@@ -0,0 +1,11 @@
+from trino.dbapi import connect
+from trino.auth import BasicAuthentication
+from sqlalchemy import create_engine
+from sqlalchemy.schema import Table, MetaData
+from sqlalchemy.sql.expression import select, text
+
+engine = create_engine('trino://xxx:zzz@emr-t.data.avedian.info:8889/avedian_dwh')
+connection = engine.connect()
+
+rows = connection.execute(text("SELECT * FROM avedian_dwh.wh_olap.grd limit 10")).fetchall()
+print(rows);
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Avedian-DL-Examples
+# Avedian-EDL-Examples
+
+## Requirements:
+- Python 3
+- Avedian EDL Credentials
+
+## Steps
+```
+cd EDL-Trino
+python3 -m venv env
+source env/bin/activate
+pip3 install -r requirements.txt
+python3 Example-DBAPI.py
+```
